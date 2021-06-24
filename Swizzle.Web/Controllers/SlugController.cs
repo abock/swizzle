6fb1d3e7c7888ab0d2e0bef57a17a43d170a7dbc
Aaron Bockover <aaron@abock.dev>
2021-06-24 17:29:33 -0400

    Don't leak non DTOs

--- Swizzle.Web/Controllers/SlugController.cs
+++ Swizzle.Web/Controllers/SlugController.cs
@@ -1,5 +1,9 @@
+using System;
+
 using Microsoft.AspNetCore.Mvc;
 
+using Swizzle.Dto;
+using Swizzle.Models;
 using Swizzle.Services;
 
 namespace Swizzle.Controllers
@@ -29,20 +33,33 @@ namespace Swizzle.Controllers
                 return Redirect($"/{items.Random().Slug}{format}");
             }
 
-            if (items.TryGetItemBySlug(slug, out var item))
-                return Ok(item);
+            Item? GetItem(string slug)
+            {
+                if (items.TryGetItemBySlug(slug, out var item))
+                    return item;
 
-            // In legacy shart, as a hint for the rewriter in nginx,
-            // each shart vhost's short links all started with a path
-            // '/<vhostFirstChar>' such as '/<c>SLUG' for <c>atoverflow.com
-            // with the slug SLUG, so support this as well to preserve
-            // any old links.
-            if (slug.Length > 1 &&
-                slug[0] == items.Key[0] &&
-                items.TryGetItemBySlug(slug[1..], out item))
-                return Ok(item);
+                // In legacy shart, as a hint for the rewriter in nginx,
+                // each shart vhost's short links all started with a path
+                // '/<vhostFirstChar>' such as '/<c>SLUG' for <c>atoverflow.com
+                // with the slug SLUG, so support this as well to preserve
+                // any old links.
+                if (slug.Length > 1 &&
+                    slug[0] == items.Key[0] &&
+                    items.TryGetItemBySlug(slug[1..], out item))
+                    return item;
 
-            return NotFound();
+                return null;
+            }
+
+            var item = GetItem(slug);
+            if (item is null)
+                return NotFound();
+
+            if (format is null ||
+                !ItemResourceKind.TryGetFromExtension(format, out _))
+                return Ok(item.ToDto(new Uri($"https://{Request.Host}/")));
+
+            return Ok(item);
         }
     }
 }
